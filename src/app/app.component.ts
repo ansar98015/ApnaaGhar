d16092a321762568221cd7ac625d41a2c5d691d8
Ansar Khan <ansarkhan98015@gmail.com>
2024-11-06 20:02:36 +0530

    header and footer component added.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MainHeaderComponent } from './components/main-header/main-header.component';
+import { MainFooterComponent } from './components/main-footer/main-footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MainHeaderComponent, MainFooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/main-footer/main-footer.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-footer',
+  standalone: true,
+  imports: [],
+  templateUrl: './main-footer.component.html',
+  styleUrls: ['./main-footer.component.css']
+})
+export class MainFooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
