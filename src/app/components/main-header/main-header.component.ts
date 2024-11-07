291684e8aef4286042d5545d74ab7874dc6a92e2
Ansar Khan <ansarkhan98015@gmail.com>
2024-11-07 20:08:42 +0530

    header complete stg1

--- src/app/components/main-header/main-header.component.ts
+++ src/app/components/main-header/main-header.component.ts
@@ -1,17 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { allPrimeNGModules } from '../../services/primeNGShared';
 
 @Component({
   selector: 'app-main-header',
   standalone: true,
-  imports: [],
+  imports: [allPrimeNGModules],
   templateUrl: './main-header.component.html',
   styleUrls: ['./main-header.component.css']
 })
 export class MainHeaderComponent implements OnInit {
 
+  headerItems: any[] | undefined;
   constructor() { }
 
   ngOnInit() {
+    this.headerItems = [
+      { label: 'Home', icon: 'pi pi-home' },
+      { label: 'Contact Us', icon: 'pi pi-envelope' }
+    ]
   }
 
 }
