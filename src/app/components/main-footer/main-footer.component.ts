1df6d0aa9c507eebf17180b2dfcd0c49e2200f55
Ansar Khan <ansarkhan98015@gmail.com>
2024-12-06 17:20:28 +0530

    footers 1st commit using developer name.

--- src/app/components/main-footer/main-footer.component.ts
+++ src/app/components/main-footer/main-footer.component.ts
@@ -1,17 +1,43 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-main-footer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './main-footer.component.html',
   styleUrls: ['./main-footer.component.css']
 })
 export class MainFooterComponent implements OnInit {
+  topCityPropertyData:any[] = [];
 
   constructor() { }
 
   ngOnInit() {
+    let cityNames = [
+      { city: "Delhi",  },
+      { city: "Mumbai",  },
+      { city: "Gurgaon",  },
+      { city: "Bangalore",  },
+      { city: "Pune",  },
+      { city: "Noida",  },
+      { city: "Lucknow",  },
+      { city: "Ghaziabad",  },
+      { city: "Navi Mumbai",  },
+      { city: "Greater Noida",  },
+      { city: "Chennai",  },
+      { city: "Thane",  },
+      { city: "Ahmedabad",  },
+      { city: "Jaipur",  },
+      { city: "Hyderabad",  }
+    ];
+
+    this.topCityPropertyData = [
+      { title: 'Property for Sale', type: 'Real estate', city: cityNames },
+      { title: 'Flats for Sale', type: 'Flats', city: cityNames },
+      { title: 'Flats for Rent', type: 'Flats for Rent', city: cityNames },
+      { title: 'New Projects', type: 'New Projects', city: cityNames },
+    ]
   }
 
 }
