6cc0f311f8a401f27907d538b2ddf73937e31549
Ansar Khan <ansarkhan98015@gmail.com>
2024-12-15 20:26:20 +0530

    filter option overlay popup and data added.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,13 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule, provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), BrowserModule, BrowserAnimationsModule]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
+     provideRouter(routes), BrowserModule, BrowserAnimationsModule,
+    provideAnimations(),
+  ]
 };
--- src/app/components/show-all/show-all.component.ts
+++ src/app/components/show-all/show-all.component.ts
@@ -1,28 +1,116 @@
 import { Component, OnInit } from '@angular/core';
 import { allPrimeNGModules } from '../../services/primeNGShared';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-show-all',
   templateUrl: './show-all.component.html',
   styleUrls: ['./show-all.component.css'],
   standalone: true,
-  imports: [allPrimeNGModules],
+  imports: [allPrimeNGModules, FormsModule],
 })
 export class ShowAllComponent implements OnInit {
   filerBtns:any[] = [];
+  propertyTypeOptions:any[] = [];
+  priceOptions:any[] = [];
+  BHKOptions:any[] = [];
+  ownershipOptions:any[] = [];
+  filterOptionsData:any[] = [];
+  popupUi:any;
+  minPrice:number = 0;
+  maxPrice:number = 1000000000;
+  priceRangeValues: number[] = [this.minPrice, this.maxPrice];
 
   constructor() { }
 
   ngOnInit() {
     this.filerBtns = [
-      {btnName: 'Residential', badgeVal: 1},
-      {btnName: 'Price', badgeVal: 3},
-      {btnName: 'BHK', badgeVal: 5},
+      {btnName: 'Property Type', badgeVal: 0},
+      {btnName: 'Price', badgeVal: 0},
+      {btnName: 'BHK', badgeVal: 0},
       {btnName: 'Ownership', badgeVal: 0}
     ]
+    this.optionsData();
   }
 
-  sidebartoggle(){
+  optionsData(){
+    this.propertyTypeOptions = [
+      { name: 'Flats', type: 'Property Type', value: false },
+      { name: 'Residential Plots', type: 'Property Type', value: false },
+      { name: 'House', type: 'Property Type', value: false },
+      { name: 'Villa', type: 'Property Type', value: false },
+      { name: 'Builder Floors', type: 'Property Type', value: false },
+      { name: 'Farm House', type: 'Property Type', value: false },
+      { name: 'Penthouse', type: 'Property Type', value: false },
+      { name: 'Agricultural Land', type: 'Property Type', value: false },
+      { name: 'Commercial Land', type: 'Property Type', value: false },
+      { name: 'Office Space', type: 'Property Type', value: false },
+      { name: 'Commercial Shops', type: 'Property Type', value: false },
+      { name: 'Hotels', type: 'Property Type', value: false },
+      { name: 'Business Center', type: 'Property Type', value: false }
+    ];
+
+    this.BHKOptions = [
+      { name: '1 RK', type: 'BHK', value: false },
+      { name: '1 BHK', type: 'BHK', value: false },
+      { name: '2 BHK', type: 'BHK', value: false },
+      { name: '3 BHK', type: 'BHK', value: false },
+      { name: '4 BHK', type: 'BHK', value: false },
+      { name: '5 BHK', type: 'BHK', value: false },
+      { name: '5+ BHK', type: 'BHK', value: false }
+    ];
+
+    this.ownershipOptions = [
+      { name: 'Agent', type: 'Ownership', value: false },
+      { name: 'Individual', type: 'Ownership', value: false },
+      { name: 'Builder', type: 'Ownership', value: false }
+    ];
   }
 
+  popupToggle(e:any, btnName:any, popup:any){
+    this.popupUi = btnName;
+    if(btnName === 'Property Type'){
+      this.filterOptionsData = this.propertyTypeOptions;
+    }else if(btnName === 'BHK'){
+      this.filterOptionsData = this.BHKOptions;
+    }else if(btnName === 'Ownership'){
+      this.filterOptionsData = this.ownershipOptions;
+    }
+    popup.toggle(e);
+  }
+
+  filterCheckBoxValueChanged(e:any, option:any){
+    if(option.type === 'Property Type'){
+      let data = this.propertyTypeOptions.find(op=>op.name === option.name);
+      data.value = e.checked;
+      this.badgeValueChange(option, this.propertyTypeOptions);
+    }else if(option.type === 'BHK'){
+      let data = this.BHKOptions.find(op=>op.name === option.name);
+      data.value = e.checked;
+      this.badgeValueChange(option, this.BHKOptions);
+    }else if(option.type === 'Ownership'){
+      let data = this.ownershipOptions.find(op=>op.name === option.name);
+      data.value = e.checked;
+      this.badgeValueChange(option, this.ownershipOptions);
+    }
+  }
+
+  badgeValueChange(option:any, data:any[]){
+    let batch = this.filerBtns.find(btn=>btn.btnName === option.type);
+    let chooseOptions = data.filter(choose=>choose.value === true);
+    batch.badgeVal = chooseOptions.length;
+  }
+
+  sliderValueChanged(e:any){
+    this.minPrice = e.values[0];
+    this.maxPrice = e.values[1];
+  }
+
+  priceValueChanged(e:any, type:string){
+    if(type === 'minPrice'){
+      this.minPrice = e.value;
+    }else{
+      this.maxPrice = e.value;
+    }
+  }
 }
