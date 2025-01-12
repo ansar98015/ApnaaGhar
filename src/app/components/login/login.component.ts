dd3bc24b5b7a191dd279f9ab4596ba45707176cb
Ansar Khan <ansarkhan98015@gmail.com>
2025-01-12 17:33:22 +0530

    member added in login form.

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -13,6 +13,7 @@ import { CreateAccountComponent } from "../create-account/create-account.compone
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent implements OnInit, AfterViewInit {
   loginForm!: FormGroup;
   pswdDetailsDialog:boolean = false;
@@ -35,38 +36,23 @@ export class LoginComponent implements OnInit, AfterViewInit {
   loginFormBuilder(){
     this.loginForm = this.fb.group({
       username: ['', [Validators.required]],
-      password: ['', [Validators.required, this.passwordValidator]],
+      password: ['', [Validators.required]],
+      member: ['', [Validators.required]],
     });
   }
 
-  passwordValidator(control: AbstractControl): { [key: string]: boolean } | null {
-    const password = control.value;
-    // Regex to check the password requirements
-    const hasUpperCase = /[A-Z]/.test(password); // At least one uppercase letter
-    const hasLowerCase = /[a-z]/.test(password); // At least one lowercase letter
-    const hasNumeric = /[0-9]/.test(password); // At least one numeric digit
-    const hasSpecialChar = /[@#$&]/.test(password); // At least one special character
-    const isValidLength = password?.length >= 8; // Minimum length is 8
-
-    if (hasUpperCase && hasLowerCase && hasNumeric && hasSpecialChar && isValidLength) {
-      return null; // Valid password
-    } else {
-      return { passwordInvalid: true }; // Invalid password
-    }
-  }
-
   onLogin(){
-    if(this.loginForm['valid']){
-
-      if((this.loginForm.value).username === 'ApnaaGhar' && (this.loginForm.value).password === 'Aghar@123'){
-        this.invalidUserPswd = false;
-        this.authService.isLogin = true;
-        this.authService.user = this.loginForm.value;
-        this.router.navigate(['/home']);
-      }else{
-        this.invalidUserPswd = true;
-      }
-
+    if( 
+      this.loginForm['valid'] 
+      && (this.loginForm.value).username === 'ApnaaGhar' 
+      && (this.loginForm.value).password === 'Aghar@123'
+    ){
+      this.invalidUserPswd = false;
+      this.authService.isLogin = true;
+      this.authService.user = this.loginForm.value;
+      this.router.navigate(['/home']);
+    }else{
+      this.invalidUserPswd = true;
     }
   }
 
